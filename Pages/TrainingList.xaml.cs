5514b84c6cdd62e50d0a0943129d48f55f402907
sangyoung <jsy91626@gmail.com>
2017-08-21 19:54:36 +0900

    트레이닝리스트 항목 추가 > 버튼클릭함수를 따로 만들어줘야 하는데 그걸 모르겠어서 클릭이벤트는 동작하지 않는상태

---
+++ Pages/TrainingList.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace Microsoft.Samples.Kinect.ControlsBasics
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.Wpf.Controls;
+    using Microsoft.Samples.Kinect.ControlsBasics.DataModel;
+
+    /// <summary>
+    /// Interaction logic for MainWindow
+    /// </summary>
+    public partial class TrainingList
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrainingList"/> class. 
+        /// </summary>
+        public TrainingList()
+        {
+            this.InitializeComponent();
+
+
+            App app = ((App)Application.Current);
+
+            // Use the default sensor
+
+            //// Add in display content
+            var sampleDataSource = SampleDataSource.GetGroup("Group-1");
+            this.itemsControl.ItemsSource = sampleDataSource;
+        }
+    }
+}
